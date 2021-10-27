f67e7d3af692cb1d1d3bd2e1eeebf4b128795e7d
Maya Nelson <mnelson30@stuy.edu>
2021-10-27 11:31:19 -0400

    s

---
+++ Day1.java
@@ -0,0 +1,19 @@
+public class Day1 {
+  public static int whatfloor(String s) {
+    int floor = 0;
+    for (int i = 0; i < s.length(); i++){
+      if (s.charAt(i) == "(") {
+        floor += 1;
+      }
+      if (s.charAt(i) == ")") {
+        floor += -1;
+      }
+    }
+    return floor;
+  }
+  public static void main(String[] args) {
+    System.out.println("(())");
+    System.out.println("((()");
+    System.out.println(")())())");
+  }
+}
